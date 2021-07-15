fe3387628db5303ba5f3e0eff0baf5c2ed9ebf89
David Westen <david.western@uwe.ac.uk>
2021-07-15 22:00:44 +0100

    Don't track changes to config_overrider.py

---
+++ config_overrider - template.py
@@ -0,0 +1,8 @@
+# Copy this file as config_overrider.py and use it to set up parameters for batch processing.
+# Each row represents one run. Each item in a row is a parameter as listed/set in config.py.
+
+new_config_values = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "shallow", 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "deep", 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "deep_smac", 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "shallow_smac", 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "linear", 0, 0, 0, 0, 0, 0, 0, 0, 0]]
